2709a359f7d393fb659a25401c1ef54b2a56dfec
aby622 <leon.schlichting@haw-hamburg.de>
2015-10-05 17:40:53 +0200

    <1740>

--- workspace/Praktikum pmp2 WS15/src/aufgabenblatt1/Student.java
+++ workspace/Praktikum pmp2 WS15/src/aufgabenblatt1/Student.java
@@ -1,5 +1,16 @@
 package aufgabenblatt1;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Student {
 
+	private String VORNAME;
+	private String NACHNAHME;
+	private int MATRIKELNUMMER;
+	private List<Pruefungsleistungen> eintraege = new LinkedList<Pruefungsleistungen>();
+
+	public void neuePruefungsleistungen(String modul, int note){
+		eintraege.add(new Pruefungsleistungen(modul, note));
+	}
 }
