7e8ebfbb40bf315f6450c53d04e54425e54ead2f
aby622 <leon.schlichting@haw-hamburg.de>
2015-10-09 13:22:01 +0200

    <09102015>

--- workspace/Praktikum pmp2 WS15/src/aufgabenblatt1/Student.java
+++ workspace/Praktikum pmp2 WS15/src/aufgabenblatt1/Student.java
@@ -1,16 +1,35 @@
 package aufgabenblatt1;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Comparator;
 
-public class Student {
+public abstract class Student<Pruefungsleistung> implements Comparable<Pruefungsleistung>, Comparator<Pruefungsleistung> {
 
 	private String VORNAME;
 	private String NACHNAHME;
 	private int MATRIKELNUMMER;
-	private List<Pruefungsleistungen> eintraege = new LinkedList<Pruefungsleistungen>();
+	private Pruefungsleistung NOTE;
+	private Pruefungsleistung MODUL;
 
-	public void neuePruefungsleistungen(String modul, int note){
-		eintraege.add(new Pruefungsleistungen(modul, note));
+	public Student(String vorname, String nachnahme, int matrikelnummer, Pruefungsleistung note,
+			Pruefungsleistung modul) {
+
+		this.VORNAME = vorname;
+		this.NACHNAHME = nachnahme;
+		this.MATRIKELNUMMER = matrikelnummer;
+		this.NOTE = note;
+		this.MODUL = modul;
+	}
+
+	public int compareTo(Pruefungsleistung student1, Pruefungsleistung student2) {
+		student1 = new Student<String>("Peter", "Lustig", 2230300, "13", "ET");
+		
+	
+		return 0;
+	}
+
+	@Override
+	public int compare(Pruefungsleistung student1, Pruefungsleistung student2) {
+
+		return 0;
 	}
 }
