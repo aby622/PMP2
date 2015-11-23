db86c2afbdc91e3f29110ffa05d669a8a6a99627
aby622 <leon.schlichting@haw-hamburg.de>
2015-11-23 13:15:55 +0100

    <neue klassen erstellt>

---
+++ workspace/Praktikum pmp2 WS15/src/aufgabenblatt3/Gleis.java
@@ -0,0 +1,16 @@
+package aufgabenblatt3;
+
+public class Gleis {
+  
+  public int gleisnummer;
+  public boolean verfuegbar;
+  
+  public int getGleisnummer(){
+  return gleisnummer;
+  }
+  
+  public boolean getVerfuegbar(){
+	return verfuegbar;
+  }
+  
+}
---
+++ workspace/Praktikum pmp2 WS15/src/aufgabenblatt3/Rangierbahnhof.java
@@ -0,0 +1,14 @@
+package aufgabenblatt3;
+
+public class Rangierbahnhof<Zug> {
+  public int anzahlGleise = 3;
+  
+  public Zug[] zuege;
+  
+  public void ausfahren(){
+	
+  }
+  public void einfahren(){
+	
+  }
+}
