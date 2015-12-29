8602b6d4d0b4ac7ca3d97cfa681a0e55fedd4827
Jannes Volkens <Jannes.Volkens@HAW-Hamburg.de>
2015-12-29 19:07:10 +0100

    <PolygonSkripting>

---
+++ workspace/.metadata/.plugins/org.eclipse.core.resources/.history/82/60e0298656ae001513dcc80150d6b7bd
@@ -0,0 +1,5 @@
+package aufgabenblatt4;
+
+public class PolygonSkripting {
+
+}
--- workspace/pm2praxis/src/aufgabenblatt4/PolygonSkripting.java
+++ workspace/pm2praxis/src/aufgabenblatt4/PolygonSkripting.java
@@ -1,5 +1,18 @@
 package aufgabenblatt4;
 
+import java.util.regex.Pattern;
+
 public class PolygonSkripting {
+  
+  /** Methode überprüft, ob der Eingegebene Befehl der Syntax entspricht. 
+   * @param befehl
+   * @return boolean
+   */
+  public boolean pruefe(String befehl) {
+
+	String rAusdruck = "bewege -> [0-9]{0,1}[0-9]{0,1}[0-9]{0,1},[0-9]{0,1}[0-9]{0,1}[0-9]{0,1}";
+	return Pattern.matches(rAusdruck, befehl);
+  }
 
 }
+
